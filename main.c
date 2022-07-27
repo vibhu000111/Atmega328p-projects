801c61d7725fad56851ed73e740a3af3d1936ffb
vibhu000111 <65100225+vibhu000111@users.noreply.github.com>
2022-07-27 10:04:53 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * ATmegaSound.c
+ *
+ * Created: 11/2/2021 1:52:30 PM
+ * Author : Vibhu
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
